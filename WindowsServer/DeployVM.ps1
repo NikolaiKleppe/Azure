9b2b20b828826d2351f6b9c4eaa2e5c285663b3c
Nikolai <nikolai__92@hotmail.com>
2018-02-04 14:47:46 +0100

    Script to deploy a Windows VM

--- WindowsServer/DeployVM.ps1
+++ WindowsServer/DeployVM.ps1
@@ -1,157 +1,80 @@
 #https://docs.microsoft.com/en-us/azure/virtual-machines/windows/tutorial-manage-vm    
 #    Create virtual machine
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Server naming:
+#AW = Azure Windows
+#T  = Test environment
+
+$cred = Get-Credential
+
+#OS
+$ComputerName = "AW-T-01"
+$VMSize       = "Standard_A1"
+
+#DISK
+$DiskName       = "disk_1"
+$DiskSize       = "128"
+$CreateOption   = "FromImage"
+$Caching        = "ReadWrite"
+$StorageAccount = "https://nikmystorageaccount.blob.core.windows.net/disks/"
+
+#NETWORK
+$NetworkInterfaceName = "NIC_01"
+#$NetworkInterfaceID  = Get-AzureRmNetworkInterface | Where-Object {$_.Name -eq $NetworkInterfaceName}
+$NetworkInterfaceID   = Find-AzureRmResource -ResourceNameContains $NetworkInterfaceName
+
+
+#RESOURCE GROUP
+$ResourceGroupName = "myResourceGroup"
+$ResourceGroup     = Get-AzureRmResourceGroup | Where-Object {$_.ResourceGroupName -eq $ResourceGroupName}
+
+
+#Initial image settings
+$vm = New-AzureRmVMConfig -VMName $ComputerName -VMSize $VMSize
+
+
+ 
+#Operating system settings
+$vm = Set-AzureRmVMOperatingSystem `
+    -VM $vm `
+    -Windows `
+    -ComputerName $ComputerName `
+    -Credential $cred `
+    -ProvisionVMAgent -EnableAutoUpdate    
+
+    
+#Image settings    
+$vm = Set-AzureRmVMSourceImage `
+    -VM $vm `
+    -PublisherName MicrosoftWindowsServer `
+    -Offer WindowsServer `
+    -Skus 2016-Datacenter `
+    -Version latest
+    
+#Disk settings
+$vm = Set-AzureRmVMOSDisk `
+    -VM $vm `
+    -Name $DiskName `
+    -DiskSizeInGB $DiskSize `
+    -CreateOption $CreateOption `
+    -Caching $Caching
+
+    
+    
+    
+#Add NIC to VMdep   
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $NetworkInterfaceID.ResourceId
+
+
+
+Try {
+    Write-Output "Creating VM"
+    New-AzureRmVM -ResourceGroupName $ResourceGroup.ResourceGroupName -Location "westeurope" -VM $vm
+}
+Catch {
+    Write-Error $_
+    Exit
+}
 
 
 
