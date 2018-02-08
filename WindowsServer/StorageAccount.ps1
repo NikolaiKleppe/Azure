5e55560c6f009de5a42ebaeee90a39d73ebc4338
Nikolai <nikolai__92@hotmail.com>
2018-02-08 21:55:59 +0100

    Link to read

---
+++ WindowsServer/StorageAccount.ps1
@@ -0,0 +1,24 @@
+$ResourceGroupName = "myResourceGroup"
+
+New-AzureRmStorageAccount `
+    -ResourceGroupName $ResourceGroupName `
+    -AccountName "nikmystorageaccount" `
+    -Location "westeurope" `
+    -SkuName Standard_RAGRS `
+    -Kind Storage
+    
+    
+    
+#Storage information
+<#
+https://docs.microsoft.com/en-us/azure/storage/common/storage-quickstart-create-account?tabs=powershell
+
+https://docs.microsoft.com/en-us/azure/storage/common/storage-create-storage-account
+
+
+https://nikmystorageaccount.blob.core.windows.net/disks/   
+    
+    
+    
+    
+#>
