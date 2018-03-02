7c8cb9a885f6b191c96d7ee1556336009c8d83a8
Nikolai <nikolai__92@hotmail.com>
2018-03-02 02:46:40 +0100

    Script to create VM images

---
+++ WindowsServer/Image/CreateImage.ps1
@@ -0,0 +1,65 @@
+#Requirements:
+#First do a sysprep on the VM. (Ref doc)
+
+#Deallocates VM
+#Set VM status to generalized
+#Create image
+
+
+
+Param(
+    [Parameter(Mandatory=$True, Position=1)] 
+    [string]$ComputerName
+)
+
+
+$ErrorActionPreference 	= "Stop"
+$ResourceGroup 			= "myResourceGroup"
+$ImageName				= "GeneralPurposeVm"
+
+
+
+
+Stop-AzureRmVM -ResourceGroupName $ResourceGroup -Name $ComputerName -Force
+Set-AzureRmVM -ResourceGroupName $ResourceGroup -Name $ComputerName -Generalized
+
+
+$GetVM = Get-AzureRmVM -Name $ComputerName -ResourceGroupName $ResourceGroup
+If (!($GetVM)) {
+	Write-Output "Cant find VM named $ComputerName"
+	Exit
+}
+
+Write-Output "Creating image configuration.."
+Try {
+		$Image = New-AzureRmImageConfig -Location "westeurope" -SourceVirtualMachineId $GetVM.ID
+		Write-Output "Image configuration done"
+		Write-Output ""
+}
+Catch {
+	Write-Error $_
+	Exit
+}
+
+Try {	
+		Write-Output "Creating the image from config"
+		New-AzureRmImage -Image $Image -ImageName $ImageName -ResourceGroupName $ResourceGroup
+		Write-Output "Image $ImageName created successfully"
+}
+Catch {
+	Write-Error $_
+	Exit
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
